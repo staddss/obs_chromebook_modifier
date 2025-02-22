5381b6c6ce7389f59b3a2bb20c8a51cb62e88aa0
Hatanaka <187852766+staddss@users.noreply.github.com>
2025-02-22 22:28:36 +0900

    Update obs_chromebook_modifier.py

--- obs_chromebook_modifier.py
+++ obs_chromebook_modifier.py
@@ -3,6 +3,12 @@
 # 置換対象のバイナリファイルのパス
 file_path = "/usr/bin/obs"
 
+# ファイルの存在確認
+import os
+if not os.path.exists(file_path):
+    print(f"ファイルが存在しません: {file_path}")
+    exit(1)
+
 # 置換するバイト列
 search_bytes  = b'/opt/google/cros-containers'
 replace_bytes = b'/00000000000000000000000000'
@@ -17,3 +23,5 @@ mod_data = binary_data.replace(search_bytes, replace_bytes)
 # 変更されたバイナリデータをファイルに書き戻す
 with open(file_path, 'wb') as file:
     file.write(mod_data)
+
+print(f"完了")
